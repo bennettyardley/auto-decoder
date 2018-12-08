beb0f7d1050ab333ed3042bacbef8ab2606cebf8
Bennett Yardley <33298178+bennettyardley@users.noreply.github.com>
2018-12-08 17:12:23 -0500

    Update decode.py

--- decode.py
+++ decode.py
@@ -1,7 +1,8 @@
 import string, binascii, enchant, re
 
 def caesar(text, shift):
-    
+    atoz = string.ascii_lowercase
+    shiftatoz = atoz[shift:] + atoz[:shift]
     table = str.maketrans(alphabet, shifted_alphabet)
     return text.translate(table)
 
